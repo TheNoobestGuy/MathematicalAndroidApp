941a716ba90345c57b7f9793f7651c7356ad8244
TheNoobestguy <maciej.kpt@gmail.com>
2024-12-14 13:25:58 +0100

    Fixing multiplication and division with power to and within functions

--- app/src/main/java/com/example/mathmaster/customviews/AdvancedKeyboard.kt
+++ app/src/main/java/com/example/mathmaster/customviews/AdvancedKeyboard.kt
@@ -774,7 +774,7 @@ class AdvancedKeyboard @JvmOverloads constructor(
         val result: PairEquation<Double, Int> = PairEquation(0.0, index)
         var iterator: Int = index
         val threshold = 1E-10
-        println(equation)
+
         while (iterator < equation.size) {
             when (equation[iterator]) {
                 is Char -> {
