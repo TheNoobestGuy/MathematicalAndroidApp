f88f8cd3d7576686d3b2489af706ffeda6592e8c
aerobat <maciej.kpt@gmail.com>
2025-02-22 22:30:31 +0100

    Almost completed script for training machine on dataset.

--- TrainModel.py
+++ TrainModel.py
@@ -1,64 +1,195 @@
 import torch
 import pickle
 import gzip
+import time
 from torch.optim import AdamW
-from transformers import LongformerModel
-
-# Load model
-dimensions = 612
-model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
-model.config.hidden_size = dimensions
-model.resize_token_embeddings(dimensions)
-
-# Optimizer
-optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01)
+from torch.utils.data import Dataset, DataLoader
+from transformers import T5ForConditionalGeneration
 
 # Check if CUDA is available, and move tensor to GPU if so
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+
+# Class for dataloader
+class QADataset(Dataset):
+    def __init__(self, answers, questions, questions_attention_masks, answers_attention_masks):
+        self.answers = answers
+        self.questions = questions
+        self.questions_attention_masks = questions_attention_masks
+        self.answers_attention_masks = answers_attention_masks
+
+    def __len__(self):
+        return len(self.questions)
+
+    def __getitem__(self, index):
+        input_ids = torch.stack(self.questions[index]).squeeze(0)
+        attention_mask = torch.stack(self.questions_attention_masks[index]).squeeze(0)
+
+        batch_size = input_ids.shape[0]
+        seq_len = input_ids.shape[2]
+
+        return  {
+            "input_ids": input_ids.reshape(batch_size, seq_len),
+            "attention_mask": attention_mask.reshape(batch_size, seq_len),
+            "labels": self.answers[index].repeat(batch_size, 1),
+            "decoder_attention_mask": self.answers_attention_masks[index].repeat(batch_size, 1)
+        }
+        
 
 # Training loop
-tokenized_questions_path = ""
-tokenized_answers_path = ""
+tokenized_questions_path = "vectorized_questions.pkl.gz"
+tokenized_answers_path = "vectorized_answers.pkl.gz"
 
-with gzip.open(tokenized_answers_path, 'rt', encoding="utf-8") as tkn_answers:
-    with gzip.open(tokenized_questions_path, 'rt', encoding="utf-8") as tkn_questions:
-        for epoch in range(2):
+questions_amount = 6
+accumulation_steps = 5
+dimension = 768
+batch_size = 1
+limit = 100
+
+for epoch in range(1):
+    # Load model
+    model = T5ForConditionalGeneration.from_pretrained('my_model').to(device)
+
+    # Optimizer
+    optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01)
+
+    with gzip.open(tokenized_answers_path, 'rb') as tkn_answers:
+        with gzip.open(tokenized_questions_path, 'rb') as tkn_questions:
+
             model.train()
             total_loss = 0
+            iterator = 0
 
-            while True:
+            while iterator < limit:
                 try:
+                    # Save model
+                    if iterator % 100000 == 0:
+                        model.save_pretrained(f"my_model_{iterator/100000}")
+
+                    # Print time
+                    if iterator % 10000 == 0:
+                        print(f"Iteration: {iterator}, Time: {time.strftime("%H:%M:%S")}")
+
                     optimizer.zero_grad()
-                    
-                    # Get data from pickle files
-                    answer = pickle.load(tkn_answers)
-                    questions = []
-                    attention_mask = []
 
-                    for _ in range(6):
-                        item = pickle.load(tkn_questions)
-                        questions.append(item)
-                        attention_mask.append(item['attention_mask'])
+                    # Batches for datasets
+                    answers_batch = []
+                    anwser_attention_mask_batch = []
 
-                    # Get input data and move them to GPU
-                    input_ids = questions.to(device)
-                    attention_mask = attention_mask.to(device)
-                    labels = answer.to(device)
+                    questions_batch = []
+                    questions_attention_masks_batch = []
 
-                     # Forward pass
-                    outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
-                    loss = outputs.loss
-                    total_loss += loss.item()
+                    # Proccess files with vectorized data
+                    for num in range(batch_size):
+                        try:
+                            # Get answer input data from pickle file
+                            tokenized_answer = pickle.load(tkn_answers)
+                            answer = tokenized_answer
 
-                    # Backward pass and optimize
-                    loss.backward()
-                    optimizer.step()
-                except:
-                    break
-            
-            # Save model and free cache
-            torch.save(model.state_dict(), 'model.pth')
-            torch.cuda.empty_cache()
+                            # Get questions input data from pickle file
+                            current_questions_batch = []
+                            current_questions_attention_masks_batch = []
 
-print("Done")
+                            for question in range(questions_amount):
+                                tokenized_question = pickle.load(tkn_questions)
+                                current_questions_batch.append(tokenized_question)
+                                del tokenized_question
+
+                            # Equalize tensors
+                            dim = 0
+                            size = 0
+                            equal = True
+                            tokenized_question_size = 0
+                            for question in current_questions_batch:
+                                if answer.shape[1] != question.shape[1]:
+                                    dim = max(answer.shape[1], question.shape[1])
+                                    equal = False
+
+                                if dim > size:
+                                    size = dim
+
+                                if question.shape[1] > tokenized_question_size:
+                                    tokenized_question_size = question.shape[1]
+                            
+                            if not equal:
+                                if size > dimension:
+                                    if tokenized_answer.shape[1] > dimension and tokenized_question_size > dimension:
+                                        anwser = tokenized_answer[:, :dimension]
+                                        for i in range(questions_amount):
+                                            current_questions_batch[i] = current_questions_batch[i][:, :dimension]
+                                    elif size == tokenized_answer.shape[1]:
+                                        anwser = tokenized_answer[:, :dimension]
+                                        for i in range(questions_amount):
+                                            current_questions_batch[i] = torch.nn.functional.pad(current_questions_batch[i], (0, dimension-current_questions_batch[i].shape[1]), value=0).to(device)
+                                    else:
+                                        answer = torch.nn.functional.pad(tokenized_answer, (0, dimension-tokenized_answer.shape[1]), value=0).to(device)
+                                        for i in range(questions_amount):
+                                            current_questions_batch[i] = current_questions_batch[i][:, :dimension]
+                                else:
+                                    answer = torch.nn.functional.pad(tokenized_answer, (0, size-tokenized_answer.shape[1]), value=0).to(device)
+                                    for i in range(questions_amount):
+                                        current_questions_batch[i] = torch.nn.functional.pad(current_questions_batch[i], (0, size-current_questions_batch[i].shape[1]), value=0).to(device)
+
+                            # Make attention masks
+                            for question in current_questions_batch:
+                                current_questions_attention_masks_batch.append((question != 0).int().to(device))
+                            current_answer_attention_mask = (answer != 0).int().to(device)
+
+                            # Push everything to proper batches
+                            answers_batch.append(answer)
+                            anwser_attention_mask_batch.append(current_answer_attention_mask)
+                            questions_batch.append(current_questions_batch)
+                            questions_attention_masks_batch.append(current_questions_attention_masks_batch)
+
+                            # Free memory
+                            del answer
+                            del current_questions_batch
+                            del current_answer_attention_mask
+                            del current_questions_attention_masks_batch
+                           
+                        except Exception as e:
+                            print(f"Error: {e}")
+                            break
+
+                    # Load the data with custom function
+                    dataset = QADataset(answers_batch, questions_batch, questions_attention_masks_batch, anwser_attention_mask_batch)
+                    train_loader = DataLoader(dataset, batch_size=1)
+
+                    # Train model
+                    for batch in train_loader:
+                        # Move the data to the GPU
+                        input_ids = batch["input_ids"].squeeze(0).to(device)
+                        attention_mask = batch["attention_mask"].squeeze(0).to(device)
+                        decoder_attention_mask = batch["decoder_attention_mask"].squeeze(0).to(device)
+                        labels = batch["labels"].squeeze(0).to(device)
+
+                        # Forward pass through the model
+                        outputs = model(
+                            input_ids=input_ids, 
+                            attention_mask=attention_mask, 
+                            decoder_attention_mask=decoder_attention_mask,
+                            labels=labels,
+                            past_key_values = None
+                            )
+                        
+                        # Handle loss
+                        if outputs.loss is not None:
+                            loss = outputs.loss
+                            loss.backward()
+                            
+                             # Accumulate gradients
+                            if (iterator + 1) % accumulation_steps == 0:
+                                optimizer.step()
+                                optimizer.zero_grad()
+
+                    # Free memory
+                    del tokenized_answer
+                    del questions_batch
+                    del questions_attention_masks_batch
+                    torch.cuda.empty_cache()
+
+                    iterator += 1
+                except Exception as e:
+                    print(f"Error: {e}")
+
+# Done
+print(f"Done, End time: {time.strftime("%H:%M:%S")}")
--- VectorizeData.py
+++ VectorizeData.py
@@ -1,53 +1,45 @@
 import json
-import numpy
 import pickle
-from transformers import LongformerTokenizer
+from transformers import T5Tokenizer
 
 # Load database
 data_file = "QA_database_EN.json"
 
-start = 0
-limit = 3500000
+# Load the tokenizer
+tokenizer = T5Tokenizer.from_pretrained('t5-large')
 
-questions =  numpy.empty((limit, 28), dtype='str')
-answers = numpy.empty(limit, dtype='str')
+questions = []
+answers = []
 
-iterator = 0
 with open(data_file, 'r') as file:
     for line in file:
         if line.strip():
-            if iterator >= start:
-                data = json.loads(line)
+            data = json.loads(line)
 
-                list = data.get('Questions')
-                for idx, question in enumerate(list):
-                    questions[iterator][idx] = question
+            list = data.get('Questions')
+
+            questions.append([])
+            for idx, question in enumerate(list):
+                questions[-1].append(question)
                     
-                answers[iterator] = data.get('Answer')
-
-            iterator += 1
-            if iterator >= limit:
-                break
+            answers.append(data.get('Answer'))
 
 print("Arrays are ready!")
 
 # Tokenize data
-dimensions = 612
-tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
-
 with open('vectorized_questions.pkl', 'ab') as f:
     for question in questions:
-        for q in question:
-            vector = tokenizer.encode(q, max_length=dimensions, padding="max_length", truncation=True, return_tensors='pt')
-            pickle.dump(vector, f)
-            del vector
+            for q in question:
+                vector = tokenizer.encode(q, return_tensors='pt')
+                pickle.dump(vector, f)
+                del vector
 
        
 print("Questions has been tokenized!")
 
 with open('vectorized_answers.pkl', 'ab') as f:
     for a in answers:
-        vector = tokenizer.encode(a, max_length=dimensions, padding="max_length", truncation=True, return_tensors='pt')
+        vector = tokenizer.encode(a, return_tensors='pt')
         pickle.dump(vector, f)
         del vector
 
